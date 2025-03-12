a60cbc4c0eeb561fb6c8037e884dbb2fdc032ce6
Andres Elacion III <andreselacioniii@gmail.com>
2025-03-12 10:15:33 +0800

    created view

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('/create-job', [JobPostingController::class, 'create'])->name('create-job');
 });
 
 require __DIR__.'/settings.php';
